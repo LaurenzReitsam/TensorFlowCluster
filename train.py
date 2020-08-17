47716efd19826faae0e536085804e5cfd82c914e
Laurenz.Reitsam <laurenz.reitsam@asset-metrix.com>
2020-08-17 17:28:43 +0200

    adding logs

--- train.py
+++ train.py
@@ -6,66 +6,63 @@ import tensorflow as tf
 import numpy as np
 import datetime
 
-try:
 
-    f = open('/usr/src/app/logs.txt', 'w')
+def logging(text):
+    with open('/usr/src/app/logs.txt', 'a') as f:
+        print(text, file=f)
 
-    print("Starting procedure", file=f)
+def mnist_dataset(batch_size):
+    logging("Loading Data.")
+    (x_train, y_train), _ = tf.keras.datasets.mnist.load_data()
+    logging("Loading Data done.")
+    x_train = x_train / np.float32(255)
+    y_train = y_train.astype(np.int64)
+    train_dataset = tf.data.Dataset.from_tensor_slices(
+        (x_train, y_train)).shuffle(1000).repeat().batch(batch_size)
+    return train_dataset
 
-    def mnist_dataset(batch_size):
-        (x_train, y_train), _ = tf.keras.datasets.mnist.load_data()
-        x_train = x_train / np.float32(255)
-        y_train = y_train.astype(np.int64)
-        train_dataset = tf.data.Dataset.from_tensor_slices(
-            (x_train, y_train)).shuffle(1000).repeat().batch(batch_size)
-        return train_dataset
+def build_and_compile_cnn_model():
+    model = tf.keras.Sequential([
+      tf.keras.Input(shape=(28, 28)),
+      tf.keras.layers.Reshape(target_shape=(28, 28, 1)),
+      tf.keras.layers.Conv2D(32, 3, activation='relu'),
+      tf.keras.layers.Flatten(),
+      tf.keras.layers.Dense(128, activation='relu'),
+      tf.keras.layers.Dense(10)
+      ])
 
-    def build_and_compile_cnn_model():
-        model = tf.keras.Sequential([
-          tf.keras.Input(shape=(28, 28)),
-          tf.keras.layers.Reshape(target_shape=(28, 28, 1)),
-          tf.keras.layers.Conv2D(32, 3, activation='relu'),
-          tf.keras.layers.Flatten(),
-          tf.keras.layers.Dense(128, activation='relu'),
-          tf.keras.layers.Dense(10)
-          ])
+    model.compile(
+      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+      optimizer=tf.keras.optimizers.SGD(learning_rate=0.001),
+      metrics=['accuracy'])
+    return model
 
-        model.compile(
-          loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-          optimizer=tf.keras.optimizers.SGD(learning_rate=0.001),
-          metrics=['accuracy'])
-        return model
+logging("Setting Strategy")
+strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
+logging("Setting Strategy done.")
 
+per_worker_batch_size = 32
+num_workers = 2
 
-    strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
+global_batch_size = per_worker_batch_size * num_workers
 
-    per_worker_batch_size = 32
-    num_workers = 2
+logging("Generating batched data.")
+multi_worker_dataset = mnist_dataset(global_batch_size)
+logging("Generating batched data done.")
 
-    global_batch_size = per_worker_batch_size * num_workers
+logging("Building Model")
+with strategy.scope():
+    multi_worker_model = build_and_compile_cnn_model()
+logging("Building Model done.")
 
-    print("Loading Data", file=f)
-    multi_worker_dataset = mnist_dataset(global_batch_size)
+# print("Create TensorBoard Callback")
+# log_dir = "/data_volume/logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+# tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-    print("Building Model", file=f)
-    with strategy.scope():
-        multi_worker_model = build_and_compile_cnn_model()
+logging("Fitting Model")
+multi_worker_model.fit(multi_worker_dataset,
+                      epochs=60,
+                      steps_per_epoch=60,
+                      callbacks=[])
 
-    # print("Create TensorBoard Callback")
-    # log_dir = "/data_volume/logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    # tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
-
-    print("Fitting Model", file=f)
-    multi_worker_model.fit(multi_worker_dataset,
-                          epochs=60,
-                          steps_per_epoch=60,
-                          callbacks=[])
-
-    print("Done!", file=f)
-
-except Exception as e:
-    print("---------------------")
-    print(e, file=f)
-
-
-f.close()
+logging("Done!")
