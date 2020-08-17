1dca0b88d13133513b0ae9af3c0c1ba56be7f9be
laurenz reitsam <laurenz.reitsam@hotmail.de>
2020-08-17 20:04:54 +0200

    delete bash file

--- run_training.sh
+++
@@ -1,5 +0,0 @@
-docker build -t tensorflow_env .
-
-docker run --name test_run -v C:/Users/Laurenz.Reitsam/Desktop/TensorFlowCluster/data_volume:/data_volume -it tensorflow_env
-
-tensorboard --logdir=C:\Users\Laurenz.Reitsam\Desktop\TensorFlowCluster\data_volume\logs
